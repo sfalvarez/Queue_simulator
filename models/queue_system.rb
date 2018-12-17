b4eab8ce91ae5934f0aa6e8ffc370c5998b6f1db
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2018-12-17 16:01:56 -0500

    client and queue_system model added

--- models/queue_system.rb
+++ models/queue_system.rb
@@ -1,17 +1,29 @@
 class QueueSystem
     
-    def initialize(queue_size)
-        @queue_size = queue_size
-        
+    def initialize(queue_quantity)
+        @queue_quantity = queue_quantity
         # if queue size > 1 multiple_queue_system else unique
+
+        if(@queue_quantity > 0)
+            self.create_queue_system();
+
+        else
+            puts "Error: You cannot create a queue system with 0 queues"
+
+        end
     end
 
-    def multiple_queue_system
-        
+    def create_queue_system
+        @queue_system = Array.new(@queue_quantity) {Array.new()}
     end
 
-    def unique_queue_system
-        
+    def add_user_to_queue(index_of_queue, user)
+        @queue_system[index_of_queue].push(user);
+    end
+
+    def remove_user_of_queue(index_of_queue)
+        @queue_system[index_of_queue].shift();
     end
 
 end
+
